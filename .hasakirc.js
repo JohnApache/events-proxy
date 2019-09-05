25e16b120139eafcdeecd6c92c2718f515f9f9e5
cjw <1468733552@qq.com>
2019-09-05 22:18:48 +0800

    测试 screener

--- .hasakirc.js
+++ .hasakirc.js
@@ -27,5 +27,69 @@ module.exports = {
                 return input && input.length > 0;
             }
         },
-    ]
+        {
+            type: 'confirm',
+            message: 'use store?',
+            name: 'useStore',
+            default: true,
+        },
+        {
+            type: 'list',
+            message: 'use redux or mobx?',
+            name: 'store',
+            when(answer) {
+                return answer.useStore
+            },
+            choices() {
+                return [
+                    'redux',
+                    'mobx'
+                ]
+            }
+        },
+        {
+            type: 'confirm',
+            message: 'use typescript?',
+            name: 'useTs',
+            default: true,
+        },
+        {
+            type: 'confirm',
+            message: 'use eslint?',
+            name: 'useEslint',
+            default: true,
+        },
+    ],
+    screener(answers) {
+        const answers = {
+            useTs: true,
+            store: 'mobx',
+            useEslint: true,
+        }
+        const include = [];
+        const exclude = [];
+        
+        if(a.useEslint) {
+            include.push(new RegExp('eslintConfig'));
+        }else {
+            exclude.push(new RegExp('eslintConfig'));
+        }
+
+        if(a.useTs) {
+            include.push(new RegExp('tsconfig.js'));
+        }else {
+            exclude.push(new RegExp('tsconfig.js'));
+        }
+
+        if(store === 'mobx') {
+            include.push(new RegExp('mobxStore'));
+            exclude.push(new RegExp('reduxStore'));
+        }else if(store === 'redux'){
+            include.push(new RegExp('reduxStore'));
+            exclude.push(new RegExp('mobxStore'));
+        }else {
+            exclude.push(new RegExp('mobxStore'));
+            exclude.push(new RegExp('reduxStore'));
+        }
+    }
 }
